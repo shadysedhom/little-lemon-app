560431b7c44050a69b3e79d733518bdcfa272d0a
Shady Sedhom <shady4@hotmail.nl>
2023-09-01 19:21:27 +0100

    Initial Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Header from './Header.js';
+import Nav from './Nav.js';
+import Main from './Main.js';
+import Footer from './Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import Logo from './Logo.svg';
+
+function Header() {
+    return(
+        <header>
+            <img src={Logo}></img>
+        </header>
+    );
+}
+
+export default Header;
