38f0878aa0e465d4156c2a9147125f8ab074dcbc
Shady Sedhom <shady4@hotmail.nl>
2023-09-01 20:29:18 +0100

    Added basic HTML structure, meta tags and favicon

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,87 @@
 function Footer() {
+    const doormatNavigation = [
+        {
+            text: "Home",
+            url: "https://www.google.com"
+        },
+        {
+            text: "About",
+            url: "https://www.google.com"
+        },
+        {
+            text: "Menu",
+            url: "https://www.google.com"
+        },
+        {
+            text: "Reservations",
+            url: "https://www.google.com"
+        },
+        {
+            text: "Order Online",
+            url: "https://www.google.com"
+        },
+        {
+            text: "Login",
+            url: "https://www.google.com"
+        },
+    ]
+
+    const contact = [
+        {
+            text: "Address",
+            url: "https://maps.google.com"
+        },
+        {
+            text: "Phone Number",
+            url: "tel:0600000000"
+        },
+        {
+            text: "Email",
+            url: "mailto:someone@example.com"
+        }
+    ]
+
+    const socials = [
+        {
+            text: "Address",
+            url: "https://maps.google.com"
+        },
+        {
+            text: "Phone Number",
+            url: "tel:0600000000"
+        },
+        {
+            text: "Email",
+            url: "mailto:someone@example.com"
+        }
+    ]
+
     return(
-        <footer></footer>
+        <footer>
+            <ul>
+                {
+                    doormatNavigation.map((element) => {
+                        return(<a href={element.url}><li>{element.text}</li></a>);
+                    })
+                }
+            </ul>
+
+            <ul>
+                {
+                    contact.map((element) => {
+                        return(<a href={element.url}><li>{element.text}</li></a>);
+                    })
+                }
+            </ul>
+
+            <ul>
+                {
+                    socials.map((element) => {
+                        return(<a href={element.url}><li>{element.text}</li></a>);
+                    })
+                }
+            </ul>
+        </footer>
     );
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,41 @@
 function Nav() {
+    const navigation = [
+        {
+            text: "Home",
+            url: "https://www.google.com"
+        },
+        {
+            text: "About",
+            url: "https://www.google.com"
+        },
+        {
+            text: "Menu",
+            url: "https://www.google.com"
+        },
+        {
+            text: "Reservations",
+            url: "https://www.google.com"
+        },
+        {
+            text: "Order Online",
+            url: "https://www.google.com"
+        },
+        {
+            text: "Login",
+            url: "https://www.google.com"
+        },
+    ]
+
     return(
-        <nav></nav>
+        <nav>
+            <ul>
+                {
+                    navigation.map((element) => {
+                        return(<a href={element.url}><li>{element.text}</li></a>);
+                    })
+                }
+            </ul>
+        </nav>
     );
 }
 
